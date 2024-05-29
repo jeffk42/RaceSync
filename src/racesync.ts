e6839caafd50c7da538a671a8129828f146177b9
Jeff Kraus <jeffk42@gmail.com>
2024-05-28 20:55:56 -0400

    fleshing out a couple of things

--- src/racesync.ts
+++ src/racesync.ts
@@ -1,16 +1,23 @@
-import { createServer } from "http";
+// import { createServer } from "http";
+const http = require("http");
+const properties = require("../config/racesync-config.json");
 
 // create the http server
-const server = createServer((req, res) => {
-  if (req.method == "GET" && req.url == "/api/test") {
+const server = http.createServer((req: any, res: any) => {
+  console.log(req);
+  if (req.method == "GET" && req.url == "/api/start") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello World!");
+    res.write("Race Sync Server has been started.");
     res.end();
   }
-
 });
 
 // set up the server port and listen for connections
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(properties.server.port, () => {
+  console.log(
+    "Server is running on " +
+      properties.server.hostname +
+      " port " +
+      properties.server.port,
+  );
 });
